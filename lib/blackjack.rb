6a96e07fe54f014795412315a108243d7a2be003
Andrew Bernier <a6bernier@gmail.com>
2018-09-03 23:08:55 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,7 +32,7 @@ def hit?(number)
  prompt_user
  get_user_input 
  if get_user_input == "s"
-   puts no
+   puts card_total
   elsif get_user_input = "h"
    puts deal_card
    card_total += deal_card
